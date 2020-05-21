03d2e4546e571c93749cfdfab736f87b89e1304b
Micheal Murray <michealmurray16@gmail.com>
2020-05-21 14:53:23 -0500

    Completed ViewPager2 Tutorial

--- app/src/main/java/com/raywenderlich/android/animaldoppelganger/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/animaldoppelganger/MainActivity.kt
@@ -35,39 +35,58 @@
 package com.raywenderlich.android.animaldoppelganger
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 /**
  * Main Screen
  */
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var doppelgangerNamesArray: Array<String>
+    private lateinit var doppelgangerNamesArray: Array<String>
 
-  //TODO:4 Define page change callback here
+    var doppelgangerPageChangeCallback = object :
+            ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            Toast.makeText(
+                    this@MainActivity,
+                    "Selected Position: $position",
+                    Toast.LENGTH_SHORT)
+                    .show()
+        }
+    }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    // Switch to AppTheme for displaying the activity
-    setTheme(R.style.AppTheme)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Switch to AppTheme for displaying the activity
+        setTheme(R.style.AppTheme)
 
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    doppelgangerNamesArray = resources.getStringArray(R.array.doppelganger_names)
+        doppelgangerNamesArray = resources.getStringArray(R.array.doppelganger_names)
 
-    //TODO:3 Wire DoppelgangerAdapter with ViewPager2 here
+        val doppelgangerAdapter = DoppelgangerAdapter(this, doppelgangerNamesArray.size)
+        doppelgangerViewPager.adapter = doppelgangerAdapter
 
-    //TODO:5 Register page change callback here
+        doppelgangerViewPager.registerOnPageChangeCallback(doppelgangerPageChangeCallback)
 
-    //TODO:7 Change ViewPager2 orientation here
+//        doppelgangerViewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
 
-    //TODO:10 Connect TabLayout and ViewPager2 here
+        TabLayoutMediator(tabLayout, doppelgangerViewPager) { tab, position ->
+            tab.text = doppelgangerNamesArray[position].split(" ")[0]
+        }.attach()
 
-    //TODO:11 Force to RTL mode
-  }
+        doppelgangerViewPager.layoutDirection = ViewPager2.LAYOUT_DIRECTION_RTL
+        tabLayout.layoutDirection = View.LAYOUT_DIRECTION_RTL
+    }
 
-  override fun onDestroy() {
-    super.onDestroy()
-    //TODO:6 Unregister page change callback here
-  }
+    override fun onDestroy() {
+        super.onDestroy()
+        doppelgangerViewPager.unregisterOnPageChangeCallback(doppelgangerPageChangeCallback)
+    }
 }
